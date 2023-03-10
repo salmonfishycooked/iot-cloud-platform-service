b8a6d4a8f7f6ac05e13ffe3c41a450f1a9cf932a
salmonfishycooked <salmonfishycooked@gmail.com>
2023-03-10 21:16:12 +0800

    修改 鉴权逻辑

--- tcp/Auth.go
+++ tcp/Auth.go
@@ -28,23 +28,48 @@ func authDevice(conn *Connection) bool {
 // @Description: 检查客户端是否发出tag
 // @return error
 func checkTag(conn *Connection) error {
-	conn.conn.SetReadDeadline(time.Now().Add(time.Duration(TIMEOUT) * time.Second)) // 对连接设置读数据的超时时间
-	recStr, _ := readFromClient(conn.conn)
-	conn.conn.SetReadDeadline(time.Time{}) // 取消读超时时间
+	// 只鉴权一次，不通过就踢掉
+	//conn.conn.SetReadDeadline(time.Now().Add(time.Duration(TIMEOUT) * time.Second)) // 对连接设置读数据的超时时间
+	//recStr, _ := readFromClient(conn.conn)
+	//conn.conn.SetReadDeadline(time.Time{}) // 取消读超时时间
+	//
+	//data := DeviceTag{}
+	//if err := json.Unmarshal([]byte(recStr), &data); err == nil {
+	//	counts := dao.QueryByTag(&model.Device{}, data.Tag)
+	//	// 在数据库中找不到该设备
+	//	if counts == 0 {
+	//		return errors.New("error")
+	//	}
+	//
+	//	conn.DeviceTag = data.Tag // 鉴权成功，设置标识tag
+	//} else {
+	//	return err
+	//}
+	//return nil
 
-	data := DeviceTag{}
-	if err := json.Unmarshal([]byte(recStr), &data); err == nil {
-		counts := dao.QueryByTag(&model.Device{}, data.Tag)
-		// 在数据库中找不到该设备
-		if counts == 0 {
-			return errors.New("error")
+	// 鉴权无数次
+	for {
+		conn.conn.SetReadDeadline(time.Now().Add(time.Duration(TIMEOUT) * time.Second)) // 对连接设置读数据的超时时间
+		recStr, _ := readFromClient(conn.conn)
+		conn.conn.SetReadDeadline(time.Time{}) // 取消读超时时间
+
+		data := DeviceTag{}
+		if err := json.Unmarshal([]byte(recStr), &data); err == nil {
+			if data.Tag == "" {
+				continue
+			}
+			counts := dao.QueryByTag(&model.Device{}, data.Tag)
+			// 在数据库中找不到该设备
+			if counts == 0 {
+				return errors.New("error")
+			}
+
+			conn.DeviceTag = data.Tag // 鉴权成功，设置标识tag
+		} else {
+			continue
 		}
-
-		conn.DeviceTag = data.Tag // 鉴权成功，设置标识tag
-	} else {
-		return err
+		return nil
 	}
-	return nil
 }
 
 // checkRepeatDevice
